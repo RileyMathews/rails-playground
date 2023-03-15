29923d1e00cb7f3e342100d496a7c5c1ba1289ff
Riley Mathews <rileymathews80@gmail.com>
2023-03-15 10:25:35 -0500

    reset scaffold

--- app/models/room.rb
+++
@@ -1,4 +0,0 @@
-class Room < ApplicationRecord
-  extend FriendlyId
-  friendly_id :title, use: :slugged
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_14_201719) do
+ActiveRecord::Schema[7.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "rooms", force: :cascade do |t|
-    t.string "title"
-    t.string "slug"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["slug"], name: "index_rooms_on_slug", unique: true
-  end
 
 end
